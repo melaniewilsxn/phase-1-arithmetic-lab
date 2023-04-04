e85c8c73c4e75ac6ddbce26f6838f61277642fff
Melanie Wilson <melaniemwilson1@gmail.com>
2023-04-03 20:03:37 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()*10);
+
+const num3 = 24;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
